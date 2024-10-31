989a5c43a66ee911be749ff91b541085a34f4289
lucikkjl <lucirodrigues734@gmail.com>
2024-10-30 21:50:00 -0300

    form

--- index.php
+++ index.php
@@ -3,9 +3,23 @@ include __DIR__ . "/header.php";
 ?>
 
 <body>
-<h1>bosta</h1>
+<form action="">
+    <h1>Faça o seu login</h1>
+    <label for="email">Seu e-mail*</label>
+    <input type="email" id="email" />
+    <label for="password">Sua senha*</label>
+    <input type="password" id="password" />
+    <input type="checkbox" />
+<p>Lembrar-me</p>
+<button>Entrar</button>
+<p>
+    Esqueceu sua senha?
+    <a href="https://www.com/">Clique aqui!</a>
+</p>
+</form>
 </body>
 
 <?php
 include __DIR__ . "/footer.php";
 ?>
+
