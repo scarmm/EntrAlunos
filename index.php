19c170d8be07d5c7cb738e09d32398b4a241101e
lucikkjl <lucirodrigues734@gmail.com>
2024-10-30 22:06:41 -0300

    formulario

--- index.php
+++ index.php
@@ -3,23 +3,22 @@ include __DIR__ . "/header.php";
 ?>
 
 <body>
-<form action="">
-    <h1>Faça o seu login</h1>
-    <label for="email">Seu e-mail*</label>
-    <input type="email" id="email" />
-    <label for="password">Sua senha*</label>
-    <input type="password" id="password" />
-    <input type="checkbox" />
-<p>Lembrar-me</p>
-<button>Entrar</button>
-<p>
-    Esqueceu sua senha?
-    <a href="https://www.com/">Clique aqui!</a>
-</p>
-</form>
+    <form action="">
+        <h1>Faça o seu login</h1>
+        <label for="email">Seu e-mail*</label>
+        <input type="email" id="email" />
+        <label for="password">Sua senha*</label>
+        <input type="password" id="password" />
+        <input type="checkbox" />
+        <p>Lembrar-me</p>
+        <button>Entrar</button>
+        <p>
+            Esqueceu sua senha?
+            <a href="https://www.com/">Clique aqui!</a>
+        </p>
+    </form>
 </body>
 
 <?php
 include __DIR__ . "/footer.php";
 ?>
-
