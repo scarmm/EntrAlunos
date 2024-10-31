86a737992ff4196c484d8a77f70ffc86097b8561
Yasmin Friedemann <yasminflsilva@gmail.com>
2024-10-31 00:27:48 -0300

    modificações

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ . "/header.php";
+?>
+
+<body>
+    <title>Login</title>
+    <form action="">
+    <div class="mb-3">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+  </div>
+  <div class="mb-3">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+    </form>
+</body>
+
+<?php
+include __DIR__ . "/footer.php";
+?>
