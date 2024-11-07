a676a9d51dd2a795d8fb3934e89beed2a37f7456
Jonathan Antonio Marcilio <jonathanmarcilio3011@gmail.com>
2024-11-06 21:23:05 -0300

    mundanças na tela inicial/index

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ include __DIR__ . "/header.php";
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-8">
-          <div class="text_align_center">  
-            <h1>Controle de Acesso com</h1><h1 class="cor_inovacao">Inovação</h1>
-            <img class="img_fluid logo_entraluno" src="assets/images/LogoEntraluno.png">
+          <div class="container_index">  
+            <h1 class="text_align_left font_size_index">Controle de Acesso com <span class="text_color"> Inovação </span></h1>
+            <img class="img_fluid logo_entraluno text_align_left" src="assets/images/LogoEntraluno.png">
           </div>
         </div>
         <div class="col-md-4 fundo">
-            <img  src="assets/images/catracaPlaceholder.png">
+            <img class="img_fluid"  src="assets/images/catracaPlaceholder.png">
         </div>
     </div>
 </div>
