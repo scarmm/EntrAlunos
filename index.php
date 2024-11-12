52811d6d247285aba94e1c71b5ef42cf5f4d1f2f
Jonathan Antonio Marcilio <jonathanmarcilio3011@gmail.com>
2024-11-12 20:26:41 -0300

    finalmente teminei a pagina inicial na gambiarra :)

--- index.php
+++ index.php
@@ -6,15 +6,21 @@ include __DIR__ . "/header.php";
 
 <div class="container-fluid">
     <div class="row">
+        
         <div class="col-md-8">
           <div class="container_index">  
-            <h1 class="text_align_left font_size_index">Controle de Acesso com <span class="text_color"> Inovação </span></h1>
+            <h1 class="text_align_left font_size_index">Controle de Acesso com <span class="text_color"> inovação </span></h1>
             <img class="img_fluid logo_entraluno text_align_left" src="assets/images/LogoEntraluno.png">
           </div>
         </div>
+        
         <div class="col-md-4 fundo">
-            <img class="img_fluid"  src="assets/images/catracaPlaceholder.png">
         </div>
+        
+        <div class="img_index">
+          <img class="img_fluid" src="assets/images/catracaPgInicial.png"> ,
+        </div>
+    
     </div>
 </div>
 
