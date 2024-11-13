c8f8aebf06d00917da16c6e81c5c42a5abd1e203
Jonathan Antonio Marcilio <jonathanmarcilio3011@gmail.com>
2024-11-12 21:22:47 -0300

    criação do forms na pagina de histórico

--- historico.php
+++ historico.php
@@ -5,37 +5,40 @@ include __DIR__ . "/header.php";
 <div class="container_historico">
   <div class="row">
     <div class="col-md-12">
-      <div class="caixaCadastro">
-        <form action="" id="form">
+      <div class="caixa_historico">
+        <div class="input_historico">
+              
+            <form action="" id="form">
         
-        <label for="form_funcionario">Funcionário</label>    
-        <input 
-            type="text" 
-            class="form-control" 
-            id="form_funcionario" 
-            name="funcionario"
-            required 
-        />
+            <label for="form_funcionario">Funcionário</label>    
+            <input 
+                type="text" 
+                class="form-control" 
+                id="form_funcionario" 
+                name="funcionario"
+                required 
+            />
         
-        <label for="form_matricula">Matrícula</label>
-        <input 
-            type="password" 
-            class="form-control" 
-            id="form_matricula" ,
-            name="matricula"
-            required 
-        />
+            <label for="form_matricula">Matrícula</label>
+            <input 
+                type="password" 
+                class="form-control" 
+                id="form_matricula" ,
+                name="matricula"
+                required 
+            />
   
-
-
+          
+        </div>
       </div>    
     </div>
+    <div class="col-md-12">
+      <div>
 
 
-
-
-
-
+      
+      </div>
+    </div>
   </div>
 </div>
 
